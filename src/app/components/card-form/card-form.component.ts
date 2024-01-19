009069fa4ed8fe3936e8d7a1827ac615503a8f5e
afzalkhan25 <afzal.khan@npstx.com>
2024-01-19 17:49:23 +0530

    complete

--- src/app/components/card-form/card-form.component.ts
+++ src/app/components/card-form/card-form.component.ts
@@ -13,19 +13,11 @@ export class CardFormComponent {
   constructor(private fb: FormBuilder ,)  {
      this.gymGroup = fb.group({
       firstName: ['', Validators.required],
-      lastName: ['' , [Validators.required , Validators.email ,   Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
-       mobileNo: ['' ,[Validators.required , Validators.pattern("[- +()0-9]{10,12}")]],
-       line1: ['', Validators.required,Validators.pattern(/^[A-Z]{5}[0-9]{4}[A-Z]$/)],
-       line2: ['', [Validators.required, Validators.pattern('^([0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}[0-9A-Z]{1})$')]],
-       address: this.fb.group({
-         line1: ['', [Validators.required]],
-         line2: ['', [Validators.required]],
-         locality: ['', [Validators.required]],
-         city: ['', [Validators.required]],
-         state: ['', [Validators.required]],
-         country: ['', [Validators.required]],
-         pinCode: ['', [Validators.required]],
-       })
+      lastName: ['' , [Validators.required , Validators.email ,]],
+       mobileNo: ['' ,[Validators.required , ]],
+       line1: ['', Validators.required,],
+       line2: ['', [Validators.required, ]],
+      
       
   });
     
